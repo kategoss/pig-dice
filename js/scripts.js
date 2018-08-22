36160c2c6c945c39c319cdb409b5b363a5d3d7f9
Kate Goss and Kendra Kelly <kate@>
2018-08-22 13:54:40 -0700

    add in submit function on Hold button and totalAccumulation function to make permTotal

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 // business logic
 var diceRolls = []; //array for roll vals
 var rollSum = 0; //starts roll count at 0
-var reducer = (accumulator, currentValue) => accumulator + currentValue;
+var addition = (accumulator, currentValue) => accumulator + currentValue;
+var permTotal = 0;
+var holdTotal = [];
+var rollTotal;
 
 // creates random roll 1-6
 var rollDice = function(rolling) {
   return Math.floor((Math.random() * 6) + 1);
 }
 
+var totalAccumulation = function(math) {
+  return rollTotal + permTotal;
+}
 
 // var addRolls = function() {
 //   rollDice + rollDice
@@ -25,18 +31,24 @@ $(document).ready(function() {
     $("#rolledDice1").text(roll1); //shows current roll val
     diceRolls.push(roll1); // adds row value to array
     $("#playbutton").addClass("hidden"); //hides play button after first click
-    // console.log(diceRolls);
   });
 
   $("#roll1").submit(function(event) {
     event.preventDefault();
     var roll1 = rollDice();  //calls rollDice function
     $("#rolledDice1").text(roll1); //shows current roll val
-    diceRolls.push(roll1); // adds row value to array
-    // console.log(diceRolls);
-    var rollTotal = diceRolls.reduce(reducer);
-    console.log(rollTotal);
+    diceRolls.push(roll1); // adds roll value to array
+    console.log(diceRolls);
+    rollTotal = diceRolls.reduce(addition);
     $("#tempTotal1").text(rollTotal);
   });
 
+
+  $("#hold1").submit(function(event) {
+    event.preventDefault();
+    var hold1 = totalAccumulation();  //calls rollDice function
+    holdTotal.push(hold1); // adds roll value total to array
+    var permTotal = holdTotal.reduce(addition);
+    $("#permTotal1").text(permTotal);
+  });
 });
