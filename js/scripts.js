5b4a8e462091daf73384daec50683b72d903c55e
Kate Goss and Kendra Kelly <kendra@>
2018-08-22 10:05:02 -0700

    add in rollDice function to randomize numbers 1-6

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 // business logic
-
+var rollDice = function(rolling) {
+  return Math.floor((Math.random() * 6) + 1);
+}
 
 // user interface logic
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+  $("#player1").submit(function(event) {
     event.preventDefault();
-
+    var roll1 = rollDice();
+    console.log(roll1);
   });
 });
