154674b6f74594e1ddbd9e6463bc3939a67323eb
Kate Goss and Kendra Kelly <kendra@>
2018-08-23 13:41:02 -0700

    get the playerRoll function to react to 1 seperately than all other pip options

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 
 
 //creates our constructor
-function Player(name, roll, temp, perm) {
+function Player(name) {
   this.name = name;
-  this.roll = roll;
-  this.tempScore = temp;
-  this.permScore = perm;
+  this.roll = 0;
+  this.tempScore = 0;
+  this.permScore = 0;
   // console.log(player1);
 }
 
@@ -20,14 +20,23 @@ function Player(name, roll, temp, perm) {
 //   )
 // }
 
-Player.prototype.playerRoll = function() {
-  if (this.roll === 1) {
+Player.prototype.playerRoll = function(roll) { //call function - working
+  console.log(this.roll);
+  if(this.roll === 1) {
     this.tempScore = 0;
-    // switch to other player
   } else {
-    return this.tempScore += this.roll;
+    this.tempScore += this.roll;
+    return;
+
   }
 }
+  //   // switch to other player
+
+
+
+
+  // }
+
 
 Player.prototype.hold = function() {
   return this.permScore += this.tempScore;
@@ -37,27 +46,27 @@ Player.prototype.hold = function() {
 // creates random roll 1-6
 var rollDice = function() {
   return Math.floor((Math.random() * 6) + 1);
-  // console.log(rollDice);
+
 }
 
 // user interface logic
 $(document).ready(function() {
-  var player1 = new Player(1, 0, 0, 0);
+  var player1 = new Player(1);
   var player2 = new Player(2);
 
   $("#roll1").submit(function(event) {
     event.preventDefault();
-    this.roll = rollDice(); //calls rollDice function
-    $("#rolledDice1").text(this.roll); //shows current roll val
+    player1.roll = rollDice(); //calls rollDice function
+    $("#rolledDice1").text(player1.roll); //shows current roll val - working
     player1.playerRoll();
-    $("#tempTotal1").text(this.tempScore);
+    $("#tempTotal1").text(player1.tempScore);
   });
 
-  $("#hold1").submit(function(event) {
-    event.preventDefault();
-    var score = player1.hold();
-    console.log("score=" + score);
-    $("#permTotal1").text(this.permScore);
-    this.tempScore = 0; //shows the total score
-  });
+  // $("#hold1").submit(function(event) {
+  //   event.preventDefault();
+  //   var score = player1.hold();
+  //   // console.log("score=" + score);
+  //   $("#permTotal1").text(this.permScore);
+  //   this.tempScore = 0; //shows the total score
+  // });
 });
