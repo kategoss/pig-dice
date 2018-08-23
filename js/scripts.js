a02b82bd171664a7374b6f87c3fa497339536c95
Kate Goss and Kendra Kelly <kate@>
2018-08-23 11:59:05 -0700

    assign prototype functions to player 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,63 @@
 // business logic
 
+
 //creates our constructor
 function Player(name, roll, temp, perm) {
   this.name = name;
-  this.roll = 0;
-  this.tempScore = 0;
-  this.permScore = 0;
+  this.roll = roll;
+  this.tempScore = temp;
+  this.permScore = perm;
+  // console.log(player1);
 }
+
+
+//-------------------------------------
+
+// var notYourTurn = function() {
+//   if ("#btnHold1") {
+//     .player1.hide();
+//   }
+//   )
+// }
+
+Player.prototype.playerRoll = function() {
+  if (this.roll === 1) {
+    this.tempScore = 0;
+    // switch to other player
+  } else {
+    return this.tempScore += this.roll;
+  }
+}
+
+Player.prototype.hold = function() {
+  return this.permScore += this.tempScore;
+  // switch to other player
+};
 
 // creates random roll 1-6
 var rollDice = function() {
   return Math.floor((Math.random() * 6) + 1);
-};
-
-//-------------------------------------
-
-
-var diceRolls = []; //array for roll vals
-var rollSum = 0; //starts roll count at 0
-var addition = (accumulator, currentValue) => accumulator + currentValue; //equation to add all integers in an array
-var permTotal = 0; //starts roll count at 0
-var holdTotal = []; //array for hold button to add to
-var rollTotal; //creates an empty variable globally for use locally
-var roll1;
-var clear;
-
-
-
-
-// takes the current turns roll total and adds it to the perm total when the hold button is used.
-var totalAccumulation = function(math) {
-  return rollTotal + permTotal;
+  // console.log(rollDice);
 }
-
-
 
 // user interface logic
 $(document).ready(function() {
-
-
-  $("#play1").submit(function(event) {
-    event.preventDefault();
-    var roll1 = rollDice();  //calls rollDice function
-    $("#rolledDice1").text(roll1); //shows current roll val
-    diceRolls.push(roll1); // adds row value to array
-    $("#playbutton").addClass("hidden"); //hides play button after first click
-  });
+  var player1 = new Player(1, 0, 0, 0);
+  var player2 = new Player(2);
 
   $("#roll1").submit(function(event) {
     event.preventDefault();
-    var roll1 = rollDice();  //calls rollDice function
-    $("#rolledDice1").text(roll1); //shows current roll val
-    diceRolls.push(roll1); // adds roll value to array
-    rollTotal = diceRolls.reduce(addition); //adds all rolls in the array
-    $("#tempTotal1").text(rollTotal); //shows the rolls total
+    this.roll = rollDice(); //calls rollDice function
+    $("#rolledDice1").text(this.roll); //shows current roll val
+    player1.playerRoll();
+    $("#tempTotal1").text(this.tempScore);
   });
 
   $("#hold1").submit(function(event) {
     event.preventDefault();
-    var hold1 = totalAccumulation();  //calls rollDice function
-    holdTotal.push(hold1); // adds roll value total to array
-    var permTotal = holdTotal.reduce(addition); //adds all totals from the holds in the array
-    $("#permTotal1").text(permTotal); //shows the total score
-    $("#tempTotal1").empty();
-    $("#rolledDice1").empty();
+    var score = player1.hold();
+    console.log("score=" + score);
+    $("#permTotal1").text(this.permScore);
+    this.tempScore = 0; //shows the total score
   });
-
-
-
 });
